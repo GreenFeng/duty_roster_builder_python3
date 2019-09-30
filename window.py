61dec5f434a5385d0383677c997a81fffca88be2
Green Hui <2717937732@qq.com>
2019-09-30 09:32:51 +0800

    Update window.py
    try add buttom

--- window.py
+++ window.py
@@ -1,5 +1,17 @@
 import tkinter as tk
 root = tk.Tk('order')
 welcome = tk.Label(root,text="welcome to order system")
-welcome.pack()
-tk.mainloop()
+welcome.pack(side=tk.TOP)
+welcome.config(fg='green')
+
+bey = tk.Label(root,text="if you'd like to leve")
+bey.config(fg='#f00101',bg='#20f0f0')
+bey.pack(expand=tk.YES,fill=tk.X)
+
+change_bg = tk.Button(root,text='chang color',command=color_change)
+change_bg.pack()
+
+root.mainloop()
+
+def color_change():
+	bey.config(bg='white')
